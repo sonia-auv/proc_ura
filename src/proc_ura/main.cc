a3537b1a3a9eb4fa57e9eecd1a686e68a45b35cb
fmassey <francis.masse05@gmail.com>
2017-05-04 15:09:13 -0400

    node stub

---
+++ src/proc_ura/main.cc
@@ -0,0 +1,37 @@
+/**
+ * \file	main.cc
+ * \author  Francis Masse <francis.masse05@gmail.com>
+ * \date	05/04/2017
+ *
+ * \copyright Copyright (c) 2017 S.O.N.I.A. All rights reserved.
+ *
+ * \section LICENSE
+ *
+ * This file is part of S.O.N.I.A. software.
+ *
+ * S.O.N.I.A. software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * S.O.N.I.A. software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with S.O.N.I.A. software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ros/ros.h>
+#include "proc_ura/proc_ura_node.h"
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "proc_ura");
+
+  ros::NodeHandlePtr nh(new ros::NodeHandle("~"));
+  proc_ura::ProcUraNode proc_ura_node{nh};
+  proc_ura_node.Spin();
+
+  return 0;
+}
---
+++ src/proc_ura/proc_ura_node.cc
@@ -0,0 +1,54 @@
+/**
+ * \file	main.cc
+ * \author  Francis Masse <francis.masse05@gmail.com>
+ * \date	05/04/2017
+ *
+ * \copyright Copyright (c) 2017 S.O.N.I.A. All rights reserved.
+ *
+ * \section LICENSE
+ *
+ * This file is part of S.O.N.I.A. software.
+ *
+ * S.O.N.I.A. software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * S.O.N.I.A. software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with S.O.N.I.A. software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "proc_ura_node.h"
+
+namespace proc_ura {
+
+//==============================================================================
+// C / D T O R S   S E C T I O N
+
+//------------------------------------------------------------------------------
+//
+ProcUraNode::ProcUraNode(const ros::NodeHandlePtr &nh) : nh_(nh) {}
+
+//------------------------------------------------------------------------------
+//
+ProcUraNode::~ProcUraNode() {}
+
+//==============================================================================
+// M E T H O D   S E C T I O N
+
+//------------------------------------------------------------------------------
+//
+void ProcUraNode::Spin() {
+  ros::Rate r(15);
+  while (ros::ok()) {
+    ros::spinOnce();
+    r.sleep();
+  }
+}
+
+}  // namespace proc_ura
